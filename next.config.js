7408e1d8b0c4ced2b5e198833ce41013cc8edee5
caelifer09 <c.baeza9@gmail.com>
2023-02-25 12:24:54 -0300

    actualizando

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ['image/avif', 'image/webp']
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
